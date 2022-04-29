d5b2149041e21df1161d5c3ee264ecd8eb19d446
Bernhard Kau <bernhard.kau@vcat.de>
2022-04-29 16:22:57 +0200

    adding a test to check if download buttons are available

---
+++ tests/e2e/specs/has-download-links.js
@@ -0,0 +1,32 @@
+// Load utilities from the e2e-test-utils package.
+import { visitAdminPage } from '@wordpress/e2e-test-utils';
+
+// Name of the test suite.
+describe( 'Find download buttons', () => {
+
+	// Flow being tested.
+	// Ideally each flow is independent and can be run separately.
+	it( 'Should load properly', async () => {
+		// Navigate the admin and performs tasks
+		// Use Puppeteer APIs to interacte with mouse, keyboard...
+		await visitAdminPage( '/admin.php', 'page=gf_entries&id=1' );
+
+		const bulkDownloadLink = await page.$x(
+			'//tr[@id="entry_row_1"]//span[@class="bulk-download"]//a'
+		);
+		expect( bulkDownloadLink.length ).not.toEqual( 0 );
+
+		const bulkDownloadAction = await page.$x(
+			'//select[@id="bulk-action-selector-top"]/option[@value="gf_bulk_download"]'
+		);
+		expect( bulkDownloadAction.length ).not.toEqual( 0 );
+
+		// Find download button on single entry.
+		await visitAdminPage( '/admin.php', 'page=gf_entries&view=entry&id=1&lid=1' );
+
+		const singleEntryBulkDownloadLink = await page.$x(
+			'//div[@id="bulk_download"]//a[@class="button"]'
+		);
+		expect( singleEntryBulkDownloadLink.length ).not.toEqual( 0 );
+	} );
+} );
