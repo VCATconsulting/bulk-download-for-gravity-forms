96eb8c9b5f3397290155b31a6c716689ecb541dd
Bernhard Kau <bernhard.kau@vcat.de>
2022-05-20 19:37:52 +0200

    adding a bootstrap file that would prepare the test page with some form entries

---
+++ tests/acceptance/bootstrap.php
@@ -0,0 +1,6 @@
+<?php
+
+#echo `cp -R tests/_data/4-9d030ebdb93eaada95e3ffc9e2fc8695 ../../uploads/gravity_forms/`;
+#echo `wp gf form import tests/_data/gravityforms-export.json`;
+#echo `wp gf entry import 4 tests/_data/form-with-upload-fields.json`;
+#echo `wp gf form list`;
